1e1a920857aba921373f85ea42b64b96995186e9
unknown <yepyepguru@gmail.com>
2020-01-04 12:33:04 -0800

    init formatting, bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = () => <div>Home</div>
+const CreateList = () => <div>Create</div>
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="app">
+
+            </div>
+            <Switch>
+                <Route path="/">
+                    <Home/>
+                </Route>
+                <Route path="/create">
+                    <CreateList/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
